0c7c9e3d0ff402f22f890a79c5c107568bdd63ac
Jorge De Pablo Martinez <jdepablo@alpha28.aulas.gsyc.urjc.es>
2018-10-08 14:41:26 +0200

    primeros pasos

---
+++ karaoke.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from smallsmilhandler import SmallSMILHandler
+
+if __name__ == "__main__":
+    try:
+        fichsmil = (sys.argv[1])
+    except ValueError or IndexError:
+        sys.exit("Usage: python3 karaoke.py file.smil.")
+
+    parser = make_parser()
+    SmilHandler = SmallSMILHandler()
+    parser.setContentHandler(SmilHandler)
+    parser.parse(open(fichsmil))
+    print(SmilHandler.get_tags())
