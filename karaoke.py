29917d2483723c755bec14169d63dfb184e1dbf4
Jorge De Pablo Martinez <jdepablo@beta13.aulas.gsyc.urjc.es>
2018-10-08 15:06:00 +0200

    primeros pasos actualización

--- karaoke.py
+++ karaoke.py
@@ -6,9 +6,12 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
+
+
 if __name__ == "__main__":
     try:
         fichsmil = (sys.argv[1])
+        ext = fichsmil.split('.')
     except ValueError or IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil.")
 
