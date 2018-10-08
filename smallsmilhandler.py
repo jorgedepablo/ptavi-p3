52e26a38df96c6354aa4c73208b9edf032398680
Jorge De Pablo Martinez <jdepablo@alpha28.aulas.gsyc.urjc.es>
2018-10-08 13:03:51 +0200

    handler de smil funcional, eje3

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,36 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class SmallSMILHandler(ContentHandler):
+
+    def __init__(self):
+        self.tags = ["root-layout", "region", "img", "audio", "textstream"]
+        self.dicAttrs = {"root-layout": ["width", "height", "background-color"],
+                        "region": ["id", "top", "left"],
+                        "img": ["src", "region", "begin", "dur"],
+                        "audio": ["src", "begin", "dur"],
+                        "textstream": ["src", "begin"]}
+        self.tagsList = []
+
+    def startElement(self, name, attrs):
+
+        if name in self.tags:
+            dicAttrs = {}
+            dicAttrs["element"] = name
+            for atribute in self.dicAttrs[name]:
+                dicAttrs[atribute] = attrs.get(atribute, "")
+            self.tagsList.append(dicAttrs)
+
+    def get_tags(self):
+        return self.tagsList
+
+
+if __name__ == "__main__":
+    parser = make_parser()
+    SmilHandler = SmallSMILHandler()
+    parser.setContentHandler(SmilHandler)
+    parser.parse(open('karaoke.smil'))
+    print(SmilHandler.get_tags())
