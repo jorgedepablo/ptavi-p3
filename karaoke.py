f39f2e36fcda4670ec249a609517939fce0db505
Jorge De Pablo MArtinez <jdepablo96@gmail.com>
2018-10-16 18:03:03 +0200

    hecho orientado a objetos PEP8 sin pasar

--- karaoke.py
+++ karaoke.py
@@ -9,25 +9,35 @@ from smallsmilhandler import SmallSMILHandler
 from urllib.request import urlretrieve
 
 
-def printElements(dicElements):
-    for tags in dicElements:
-        attrs_list = []
-        for tag in tags:
-            if tag != 'Tag' and tags[tag] !='':
-                attrs_list += ('\t', tag, '=', '"', tags[tag], '"')
-        print(tags['Tag'], "".join(attrs_list))
+class KaraokeLocal():
 
-def changeFormat(dicElements, fichsmil):
-    fichjson = fichsmil.split('.')[0] + '.json'
-    json.dump(dicElements, open(fichjson, 'w'), separators=(',', ':'), indent=4)
+    def __init__(self, fichsmil):
+        parser = make_parser()
+        SmilHandler = SmallSMILHandler()
+        parser.setContentHandler(SmilHandler)
+        parser.parse(open(fichsmil))
+        self.elements = SmilHandler.get_tags()
 
-def dowloandElements(dicElements):
-    for tags in dicElements:
-        for tag in tags:
-            if tag == 'src' and tags[tag].startswith('http://'):
-                location = tags[tag].split('/')[-1]
-                urlretrieve(tags[tag], location)
-                tags[tag] = location
+    def __str__(self):
+        for tags in self.elements:
+            attrs_list = []
+            for tag in tags:
+                if tag != 'Tag' and tags[tag] !='':
+                    attrs_list += ('\t', tag, '=', '"', tags[tag], '"')
+            print(tags['Tag'], "".join(attrs_list))
+
+    def to_json(self, fichsmil, fichjson):
+        if fichjson == '':
+            fichjson = fichsmil.split('.')[0] + '.json'
+        json.dump(self.elements, open(fichjson, 'w'), separators=(',', ':'), indent=4)
+
+    def do_local(self):
+        for tags in self.elements:
+            for tag in tags:
+                if tag == 'src' and tags[tag].startswith('http://'):
+                    location = tags[tag].split('/')[-1]
+                    urlretrieve(tags[tag], location)
+                    tags[tag] = location
 
 
 if __name__ == "__main__":
@@ -35,11 +45,9 @@ if __name__ == "__main__":
         fichsmil = (sys.argv[1])
     except ValueError or IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil.")
-
-    parser = make_parser()
-    SmilHandler = SmallSMILHandler()
-    parser.setContentHandler(SmilHandler)
-    parser.parse(open(fichsmil))
-    printElements(SmilHandler.get_tags())
-    changeFormat(SmilHandler.get_tags(), fichsmil)
-    dowloandElements(SmilHandler.get_tags())
+    karaoke = KaraokeLocal(fichsmil)
+    karaoke.__str__()
+    karaoke.to_json(fichsmil, '')
+    karaoke.do_local()
+    karaoke.to_json(fichsmil, 'local.json')
+    karaoke.__str__()
