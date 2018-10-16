a2cad70b3c9fd18cc288c03aa4fdb42bd48d8c03
Jorge De Pablo MArtinez <jdepablo96@gmail.com>
2018-10-16 19:04:44 +0200

    trato de pasar pep8, no soy capaz de quitar un error

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,19 +4,20 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-        self.tags = ["root-layout", "region", "img", "audio", "textstream"]
-        self.dicAttrs = {"root-layout": ["width", "height", "background-color"],
-                        "region": ["id", "top", "left"],
-                        "img": ["src", "region", "begin", "dur"],
-                        "audio": ["src", "begin", "dur"],
-                        "textstream": ["src", "begin"]}
+        self.tags = ['root-layout', 'region', 'img', 'audio', 'textstream']
+        self.dicAttrs = {'root-layout': ['width', 'height',
+                                'background-color'],
+                                'region': ['id', 'top', 'left'],
+                                'img': ['src', 'region', 'begin', 'dur'],
+                                'audio': ['src', 'begin', 'dur'],
+                                'textstream': ['src', 'begin']}
         self.tagsList = []
 
     def startElement(self, name, attrs):
-
         if name in self.tags:
             dicAttrs = {}
             dicAttrs["Tag"] = name
